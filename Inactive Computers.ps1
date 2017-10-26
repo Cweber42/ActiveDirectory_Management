2e5d57d0b27dd238477959df45234f6d7956b4e5
Cweb <xeniogame@gmail.com>
2017-10-26 10:16:03 -0500

    Update Inactive Computers.ps1

--- Inactive Computers.ps1
+++ Inactive Computers.ps1
@@ -3,7 +3,7 @@
 # Readable conversion of the lastLogonTimeStamp (as far as I am able to discern.  More details about the timestamp can
 # be found at technet - http://bit.ly/YpGWXJ  --MWT, 03/12/13
 
-$then = (Get-Date).AddDays(0) # The 60 is the number of days from today since the last logon.
+$then = (Get-Date).AddDays(0) # The -60 is the number of days from today since the last logon.
 
 Get-ADComputer -Property Name,lastLogonDate -Filter {lastLogonDate -lt $then} | FT Name,lastLogonDate
 
